f920ae61cf5f71d53943a6b43ff2210ad71e8793
Morytala <theBest@Morytalas-Mac.local>
2014-12-20 08:31:07 -0800

    Initial Commit

---
+++ UITableViewController Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Morytala on 12/20/14.
+//  Copyright (c) 2014 Morytala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/CCViewController.m
@@ -0,0 +1,29 @@
+//
+//  CCViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Morytala on 12/20/14.
+//  Copyright (c) 2014 Morytala. All rights reserved.
+//
+
+#import "CCViewController.h"
+
+@interface CCViewController ()
+
+@end
+
+@implementation CCViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
